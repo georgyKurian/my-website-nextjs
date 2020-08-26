be8d238ac5d4bb815fd2f0c67a1073b2db94c572
Georgi Varghese Kurian <georgy.v.k@gmail.com>
2020-08-26 09:19:20 -0400

    FIxed css purging and some performance improvement

--- src/components/Layout/DefaultLayout.jsx
+++ src/components/Layout/DefaultLayout.jsx
@@ -13,6 +13,7 @@ const DefaultLayout = ({
       <Head>
         <title>{`E | ${title}`}</title>
         <meta charSet="utf-8" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
       </Head>
